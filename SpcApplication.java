0e27d1a4ce1ae95089df8f94a32c18906a10de56
KunamsettiVaishnavi <71434509+KunamsettiVaishnavi@users.noreply.github.com>
2021-06-01 13:53:20 +0530

    Add files via upload

---
+++ SpcApplication.java
@@ -0,0 +1,35 @@
+package com.g3.spc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+@SpringBootApplication
+public class SpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpcApplication.class, args);
+	}
+	@Bean
+	public Docket docket()
+	{
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).select().build();
+	}
+	
+	private ApiInfo apiInfo()
+	{
+		return new ApiInfoBuilder().title("Parent Teacher Communication App").description("Parent can review student details")
+				.version("spcappV1.1").build();
+	}
+
+
+}
